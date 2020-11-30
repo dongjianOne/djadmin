e50f4588ee2fb7a4477754d20f34efda7ade48d1
Dongjian <dongjian@ysstech.com>
2020-11-30 18:20:28 +0800

    定义验证码接口

--- djadmin-system/src/main/java/com/dong/sys/modules/security/rest/AuthroizationController.java
+++ djadmin-system/src/main/java/com/dong/sys/modules/security/rest/AuthroizationController.java
@@ -1,4 +1,19 @@
 package com.dong.sys.modules.security.rest;
 
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "系统：系统授权接口")
 public class AuthroizationController {
+
+    @ApiOperation("获取验证码")
+    @GetMapping("/code")
+    public ResponseEntity<Object> getCode() {
+        return ResponseEntity.ok(null);
+    }
 }
